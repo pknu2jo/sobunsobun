8de5a845781c4f6b762df54c20cec7d8e2316447
mingming <ming9396@naver.com>
2023-05-09 19:45:00 +0900

    .

--- src/main/java/com/example/mapper/km/CustomerMapper.java
+++ src/main/java/com/example/mapper/km/CustomerMapper.java
@@ -2,6 +2,7 @@ package com.example.mapper.km;
 
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import com.example.dto.Customer;
 
@@ -12,5 +13,10 @@ public interface CustomerMapper {
     @Insert({ " INSERT INTO customer(id, pw, name, phone, email, nickname, gender) ", 
     " VALUES(#{id}, #{pw}, #{name}, #{phone}, #{email}, #{nickname}, #{gender}) " })
     public int joinCustomer(Customer customer);
+
+    // 회원정보 불러오기
+    // @Select({" SELECT m.id, m.name, m.phone, m.email, m.nickname, m.gender ",  
+    //          " FROM Customer m WHERE m.id=#{id} "})
+    public Customer selectCustomerOne(String id);
     
 }
