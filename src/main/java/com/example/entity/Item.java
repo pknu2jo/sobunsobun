5173d6d8d8dbbd76afbdc31e77f1acebf041243b
gulbae <esw2782@gmail.com>
2023-05-15 14:43:15 +0900

    0515

---
+++ src/main/java/com/example/entity/Item.java
@@ -0,0 +1,24 @@
+// package com.example.entity.sobun;
+
+// import java.util.Date;
+
+// import javax.persistence.Entity;
+// import javax.persistence.SequenceGenerator;
+// import javax.persistence.Table;
+
+// import lombok.Data;
+
+// @Data
+// @Entity
+// @Table(name = "ITEM")
+// @SequenceGenerator(name = "SEQ_ITEM_NO", sequenceName = "SEQ_ITEM_NO", initialValue = 1, allocationSize = 1)
+// public class Item {
+    
+//     private long no;
+//     private String name;
+//     private long price;
+//     private long quantity;
+//     private Date regdate;
+//     private String regNo;
+//     private long scategoryCode;
+// }
