989b3f973d86fdd49cd1842d724ff4700037378b
9aramkim <rlarkfka0113@naver.com>
2023-05-16 16:47:29 +0900

    5-16

--- src/main/java/com/example/controller/gr/MypageController.java
+++ src/main/java/com/example/controller/gr/MypageController.java
@@ -1,8 +1,15 @@
 package com.example.controller.gr;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
+@Slf4j
+@RequestMapping(value = "/customer")
+@RequiredArgsConstructor
 public class MypageController {
 
 }
