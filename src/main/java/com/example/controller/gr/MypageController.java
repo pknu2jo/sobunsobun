b2b7ae2aafff3c3168bed2686c6096653854c971
9aramkim <rlarkfka0113@naver.com>
2023-05-17 18:18:58 +0900

    5-17
    마이페이지 헤더 2줄완성ㅋㅋㅋ

--- src/main/java/com/example/controller/gr/MypageController.java
+++ src/main/java/com/example/controller/gr/MypageController.java
@@ -1,6 +1,7 @@
 package com.example.controller.gr;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
@@ -11,5 +12,10 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping(value = "/customer")
 @RequiredArgsConstructor
 public class MypageController {
+
+    @GetMapping(value = "/mypage.do")
+    public String mypageGET() {
+        return "/gr/customer/mypage";
+    }
 
 }
--- src/main/java/com/example/sobunsobun/SobunsobunApplication.java
+++ src/main/java/com/example/sobunsobun/SobunsobunApplication.java
@@ -8,9 +8,8 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-
 @SpringBootApplication
-<<<<<<< HEAD
+
 @MapperScan(basePackages = { "com.example.mapper" }) // 매퍼 위치 설정
 @ComponentScan(basePackages = { "com.example.controller",
 		"com.example.controller.gr",
@@ -19,16 +18,16 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 		"com.example.service",
 		"com.example.service.gr",
 		"com.example.config" }) // 컨트롤러, 서비스, 시큐리티 위치 설정
-=======
-@PropertySource(value = {"classpath:/global.properties"})
-@MapperScan(basePackages = {"com.example.mapper"}) // 매퍼 위치 설정
-@ComponentScan(basePackages = {"com.example.controller",
-								"com.example.restController",
-							    "com.example.service",
-								"com.example.config"}) // 컨트롤러, 서비스, 시큐리티 위치 설정
-@EntityScan(basePackages = {"com.example.entity"}) // 엔티티 위치		
-@EnableJpaRepositories(basePackages = {"com.example.repository"}) // 저장소 위치
->>>>>>> main
+
+@PropertySource(value = { "classpath:/global.properties" })
+@MapperScan(basePackages = { "com.example.mapper" }) // 매퍼 위치 설정
+@ComponentScan(basePackages = { "com.example.controller",
+		"com.example.restController",
+		"com.example.service",
+		"com.example.config" }) // 컨트롤러, 서비스, 시큐리티 위치 설정
+@EntityScan(basePackages = { "com.example.entity" }) // 엔티티 위치
+@EnableJpaRepositories(basePackages = { "com.example.repository" }) // 저장소 위치
+
 public class SobunsobunApplication {
 
 	public static void main(String[] args) {
