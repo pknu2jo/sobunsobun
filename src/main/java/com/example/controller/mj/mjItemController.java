46d1bae602aa39f58431faeb340036ec9aa31e9d
mjj <esw2782@gmail.com>
2023-05-17 23:58:57 +0900

    연동 테스트

--- src/main/java/com/example/controller/mj/mjItemController.java
+++ src/main/java/com/example/controller/mj/mjItemController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.dto.Category;
 import com.example.entity.Item;
