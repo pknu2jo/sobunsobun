08c3ffd8342f30c903c7262148b071d70a05372c
9aramkim <rlarkfka0113@naver.com>
2023-05-18 12:59:29 +0900

    5-18
    마이페이지 하는둥

--- src/main/java/com/example/config/SecurityConfig.java
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -27,118 +28,119 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    final SecurityCustomerSeviceImpl userCustomerDetailsService;
-    final SecuritySellerSeviceImpl userSellerDetailsService;
-    final SecurityAdminSeviceImpl userAdminDetailsService;
+        final SecurityCustomerSeviceImpl userCustomerDetailsService;
+        final SecuritySellerSeviceImpl userSellerDetailsService;
+        final SecurityAdminSeviceImpl userAdminDetailsService;
 
-    @Bean // 객체를 생성함
-    @Order(value = 1) // 고객
-    public SecurityFilterChain filterChain1(HttpSecurity http) throws Exception {
-        log.info("SecurityConfig => {}", "start filter chain1");
+        @Bean // 객체를 생성함
+        @Order(value = 1) // 고객
+        public SecurityFilterChain filterChain1(HttpSecurity http) throws Exception {
+                log.info("SecurityConfig => {}", "start filter chain1");
 
-        http.antMatcher("/customer/login.do")
-                .antMatcher("/customer/loginaction.do")
-                .authorizeRequests().anyRequest().authenticated().and();
+                http.antMatcher("/customer/login.do")
+                                .antMatcher("/customer/loginaction.do")
+                                .authorizeRequests().anyRequest().authenticated().and();
 
-        // 로그인 처리
-        http.formLogin()
-                .loginPage("/customer/login.do")
-                .loginProcessingUrl("/customer/loginaction.do")
-                .usernameParameter("id")
-                .passwordParameter("pw")
-                .defaultSuccessUrl("/customer/home.do")
-                .permitAll();
+                // 로그인 처리
+                http.formLogin()
+                                .loginPage("/customer/login.do")
+                                .loginProcessingUrl("/customer/loginaction.do")
+                                .usernameParameter("id")
+                                .passwordParameter("pw")
+                                .defaultSuccessUrl("/customer/home.do")
+                                .permitAll();
 
-        // 서비스 등록
-        http.userDetailsService(userCustomerDetailsService);
+                // 서비스 등록
+                http.userDetailsService(userCustomerDetailsService);
 
-        return http.build();
-    }
+                return http.build();
+        }
 
-    @Bean // 객체를 생성함
-    @Order(value = 2) // 업체
-    public SecurityFilterChain filterChain2(HttpSecurity http) throws Exception {
+        @Bean // 객체를 생성함
+        @Order(value = 2) // 업체
+        public SecurityFilterChain filterChain2(HttpSecurity http) throws Exception {
 
-        log.info("SecurityConfig => {}", "start filter chain2");
+                log.info("SecurityConfig => {}", "start filter chain2");
 
-        http.antMatcher("/seller/login.do")
-                .antMatcher("/seller/loginaction.do")
-                .authorizeRequests().anyRequest().authenticated().and();
+                http.antMatcher("/seller/login.do")
+                                .antMatcher("/seller/loginaction.do")
+                                .authorizeRequests().anyRequest().authenticated().and();
 
-        // 업체 로그인 처리
-        http.formLogin()
-                .loginPage("/seller/login.do")
-                .loginProcessingUrl("/seller/loginaction.do")
-                .usernameParameter("no")
-                .passwordParameter("pw")
-                .defaultSuccessUrl("/seller/home.do")
-                .permitAll();
+                // 업체 로그인 처리
+                http.formLogin()
+                                .loginPage("/seller/login.do")
+                                .loginProcessingUrl("/seller/loginaction.do")
+                                .usernameParameter("no")
+                                .passwordParameter("pw")
+                                .defaultSuccessUrl("/seller/home.do")
+                                .permitAll();
 
-        // 서비스 등록
-        http.userDetailsService(userSellerDetailsService);
+                // 서비스 등록
+                http.userDetailsService(userSellerDetailsService);
 
-        return http.build();
-    }
+                return http.build();
+        }
 
-    @Bean // 객체를 생성함
-    @Order(value = 3) // 관리자
-    public SecurityFilterChain filterChain3(HttpSecurity http) throws Exception {
+        @Bean // 객체를 생성함
+        @Order(value = 3) // 관리자
+        public SecurityFilterChain filterChain3(HttpSecurity http) throws Exception {
 
-        log.info("SecurityConfig => {}", "start filter chain3");
+                log.info("SecurityConfig => {}", "start filter chain3");
 
-        // 권한 설정
-        // http.authorizeRequests().anyRequest().permitAll();
-        http.authorizeRequests()
-                .antMatchers("/customer/join.do", "/customer/home.do", "/customer/login.do", "/customer/kmtest.do",
-                        "/customer/mypage.do")
-                .permitAll()
-                .antMatchers("/seller/join.do").permitAll()
-                .antMatchers("/admin/join.do").permitAll()
+                // 권한 설정
+                // http.authorizeRequests().anyRequest().permitAll();
+                http.authorizeRequests()
+                                .antMatchers("/customer/join.do", "/customer/home.do", "/customer/login.do",
+                                                "/customer/kmtest.do",
+                                                "/customer/mypage.do")
+                                .permitAll()
+                                .antMatchers("/seller/join.do").permitAll()
+                                .antMatchers("/admin/join.do").permitAll()
 
-                .antMatchers("/admin", "/admin/*").hasAuthority("ADMIN") // 주소가 9090/ROOT/admin ~~ 모든것
-                .antMatchers("/seller", "/seller/*").hasAnyAuthority("ADMIN", "SELLER")
-                .antMatchers("/customer", "/customer/*").hasAnyAuthority("CUSTOMER")
+                                .antMatchers("/admin", "/admin/*").hasAuthority("ADMIN") // 주소가 9090/ROOT/admin ~~ 모든것
+                                .antMatchers("/seller", "/seller/*").hasAnyAuthority("ADMIN", "SELLER")
+                                .antMatchers("/customer", "/customer/*").hasAnyAuthority("CUSTOMER")
 
-                .anyRequest().permitAll();
+                                .anyRequest().permitAll();
 
-        // 403 페이지 설정 (접근 권한 불가 시 표시할 화면 )
-        http.exceptionHandling().accessDeniedPage("/error/403page.do");
+                // 403 페이지 설정 (접근 권한 불가 시 표시할 화면 )
+                http.exceptionHandling().accessDeniedPage("/error/403page.do");
 
-        // 관리자 로그인 처리
-        http.formLogin()
-                .loginPage("/admin/login.do")
-                .loginProcessingUrl("/admin/loginaction.do")
-                .usernameParameter("id")
-                .passwordParameter("pw")
-                .defaultSuccessUrl("/admin/home.do")
-                .permitAll();
+                // 관리자 로그인 처리
+                http.formLogin()
+                                .loginPage("/admin/login.do")
+                                .loginProcessingUrl("/admin/loginaction.do")
+                                .usernameParameter("id")
+                                .passwordParameter("pw")
+                                .defaultSuccessUrl("/admin/home.do")
+                                .permitAll();
 
-        // 로그아웃 처리 (고객, 업체, 관리자 모두 해당)
-        http.logout()
-                .logoutUrl("/logout.do")
-                .logoutSuccessHandler(new CustomLogoutHandler())
-                .invalidateHttpSession(true)
-                .clearAuthentication(true)
-                .permitAll();
+                // 로그아웃 처리 (고객, 업체, 관리자 모두 해당)
+                http.logout()
+                                .logoutUrl("/logout.do")
+                                .logoutSuccessHandler(new CustomLogoutHandler())
+                                .invalidateHttpSession(true)
+                                .clearAuthentication(true)
+                                .permitAll();
 
-        // post는 csrf를 전송해야하지만, 주소가 /api로 시작하는 모든url은 csrf가 없어도 됨
-        http.csrf().ignoringAntMatchers("/api/**");
+                // post는 csrf를 전송해야하지만, 주소가 /api로 시작하는 모든url은 csrf가 없어도 됨
+                http.csrf().ignoringAntMatchers("/api/**");
 
-        // 서비스 등록
-        http.userDetailsService(userAdminDetailsService);
+                // 서비스 등록
+                http.userDetailsService(userAdminDetailsService);
 
-        return http.build();
-    }
+                return http.build();
+        }
 
-    // 정적 자원에는 시큐리티 필터 규칙을 적용하지 않도록 설정
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
-    }
+        // 정적 자원에는 시큐리티 필터 규칙을 적용하지 않도록 설정
+        @Bean
+        public WebSecurityCustomizer webSecurityCustomizer() {
+                return (web) -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+        }
 
-    // 회원가입과 로그인에서 사용하는 암호화 알고리즘은 동일한 것을 사용해야한다.
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+        // 회원가입과 로그인에서 사용하는 암호화 알고리즘은 동일한 것을 사용해야한다.
+        @Bean
+        public PasswordEncoder passwordEncoder() {
+                return new BCryptPasswordEncoder();
+        }
 }
