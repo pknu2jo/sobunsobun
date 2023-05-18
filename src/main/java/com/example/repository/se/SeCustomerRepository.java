4460e9f5f90d710f18a89d8faed027e709fcc9c1
ohsangeun <xxbloxxom@gmail.com>
2023-05-18 20:02:49 +0900

    230518

--- src/main/java/com/example/repository/se/SeCustomerRepository.java
+++ src/main/java/com/example/repository/se/SeCustomerRepository.java
@@ -3,11 +3,15 @@ package com.example.repository.se;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.entity.Customer;
+import com.example.entity.CustomerEntity;
 
 @Repository
-public interface SeCustomerRepository extends JpaRepository<Customer, String> {
+public interface SeCustomerRepository extends JpaRepository<CustomerEntity, String> {
 
+    // 아이디 중복확인
     int countById(String id);
+
+    // 아이디 찾기
+    CustomerEntity findByNameAndEmail(String name, String email);
     
 }
--- src/main/java/com/example/service/se/SeCustomerServiceImpl.java
+++ src/main/java/com/example/service/se/SeCustomerServiceImpl.java
@@ -2,7 +2,9 @@ package com.example.service.se;
 
 import org.springframework.stereotype.Service;
 
-import com.example.entity.Customer;
+import com.example.entity.CustomerAddressEntity;
+import com.example.entity.CustomerEntity;
+import com.example.repository.se.SeCustomerAddressRepository;
 import com.example.repository.se.SeCustomerRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -12,14 +14,16 @@ import lombok.RequiredArgsConstructor;
 public class SeCustomerServiceImpl implements SeCustomerService {
     
     final SeCustomerRepository cRepository;
+    final SeCustomerAddressRepository caRepository;
 
     // ----------------------------------------------------------------------------------------------------------
     // 회원가입
     @Override
-    public int joinCustomerOne(Customer obj) {
+    public int joinCustomerOne(CustomerEntity obj, CustomerAddressEntity obj2) {
         try {
-            Customer ret = cRepository.save(obj);
-            if( ret != null ){
+            CustomerEntity ret = cRepository.save(obj);
+            CustomerAddressEntity ret2 = caRepository.save(obj2);
+            if( ret != null && ret2 != null ){
                 return 1;
             }
             else {
@@ -42,5 +46,19 @@ public class SeCustomerServiceImpl implements SeCustomerService {
             return -1;
         }
     }
+
+    // ----------------------------------------------------------------------------------------------------------
+    // 아이디 찾기
+    @Override
+    public CustomerEntity findId(CustomerEntity customerEntity) {
+        try {
+            return cRepository.findByNameAndEmail(customerEntity.getName(), customerEntity.getEmail());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
 
 }
