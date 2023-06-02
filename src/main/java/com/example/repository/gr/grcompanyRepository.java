462b85837a882439f423745a9ec7d22a14ec6c75
9aramkim <rlarkfka0113@naver.com>
2023-06-02 17:26:26 +0900

    6-2
    css수정중

---
+++ src/main/java/com/example/repository/gr/grcompanyRepository.java
@@ -0,0 +1,15 @@
+package com.example.repository.gr;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.entity.SellerEntity;
+
+@Repository
+public interface grcompanyRepository extends JpaRepository<SellerEntity, String> {
+
+    List<SellerEntity> findAll();
+
+}
