d54bd27b62376b74ea1d896efa196f90a3686d14
9aramkim <rlarkfka0113@naver.com>
2023-06-05 18:00:04 +0900

    6-5
    수정

--- src/main/java/com/example/repository/gr/grgrpurchaseviewRepository.java
+++ src/main/java/com/example/repository/gr/grgrpurchaseviewRepository.java
@@ -6,12 +6,12 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.entity.gr.grpurchaseview;
+import com.example.entity.gr.grgrpurchaseview;
 
 @Repository
-public interface grgrpurchaseviewRepository extends JpaRepository<grpurchaseview, String> {
+public interface grgrpurchaseviewRepository extends JpaRepository<grgrpurchaseview, String> {
 
     // 내가 주문한 상품 목록
-    List<grpurchaseview> findByMemid(String id);
+    List<grgrpurchaseview> findByMemid(String id);
 
 }
