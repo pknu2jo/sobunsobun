4ef97b2e68feff9d27c0468017320119b1fc7d37
9aramkim <rlarkfka0113@naver.com>
2023-06-05 15:25:49 +0900

    6-5
    홈화면 수정중

--- src/main/java/com/example/repository/gr/grcustomerRepository.java
+++ src/main/java/com/example/repository/gr/grcustomerRepository.java
@@ -1,9 +1,12 @@
 package com.example.repository.gr;
 
+import java.math.BigDecimal;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.entity.CustomerEntity;
@@ -14,4 +17,14 @@ public interface grcustomerRepository extends JpaRepository<CustomerEntity, Stri
     List<CustomerEntity> findAll();
 
     // CustomerEntity findById();
+
+    @Query(value = "SELECT COUNT(*) FROM Customer WHERE DATE_TRUNC('day', regdate) = CURRENT_DATE", nativeQuery = true)
+    int countTodayCustomers();
+
+    @Query(value = "SELECT COUNT(*) FROM Customer WHERE quitchk = 1", nativeQuery = true)
+    int countByQuitchk();
+
+    @Query(value = "SELECT COUNT(*) FROM PURCHASESTATUS WHERE DATE_TRUNC('day', regdate) = CURRENT_DATE AND STATE=1 AND CANCEL=0", nativeQuery = true)
+    int coutByPurchase();
+
 }
