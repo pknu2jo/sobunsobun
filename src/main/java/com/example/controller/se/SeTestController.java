3a5b67a2e7dae047ea1373945a39de8181eee883
ohsangeun <xxbloxxom@gmail.com>
2023-06-07 11:38:05 +0900

    230607

--- src/main/java/com/example/controller/se/SeTestController.java
+++ src/main/java/com/example/controller/se/SeTestController.java
@@ -5,11 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @RequestMapping(value = "/customer")
-@Slf4j
 @RequiredArgsConstructor
 public class SeTestController {
 
