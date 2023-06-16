904882cf28da39980040b750a4345495d715ff44
mingming <ming9396@naver.com>
2023-06-16 17:07:54 +0900

    통합 페이지 제작 완료

--- src/main/java/com/example/service/SecurityAdminSeviceImpl.java
+++ src/main/java/com/example/service/SecurityAdminSeviceImpl.java
@@ -33,7 +33,7 @@ public class SecurityAdminSeviceImpl implements UserDetailsService {
             return User.builder()
                     .username(admin.getId())
                     .password(admin.getPw())
-                    .roles("ADMIN", "SELLER", "CUSTOMER")
+                    .roles("ADMIN")
                     .build();
         }
 
